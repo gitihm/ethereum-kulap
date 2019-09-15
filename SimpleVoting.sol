7aa89ff83a74ae0e09fd9dc8ea3f26674aecd1f5
ihm <32189851+gitihm@users.noreply.github.com>
2019-09-15 15:51:17 +0700

    Update SimpleVoting.sol

--- SimpleVoting.sol
+++ SimpleVoting.sol
@@ -1,26 +1,31 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
+contract Voting {
+  mapping (string => uint256) votesReceived;
 
-contract HelloWorld {
-    uint256 public age = 22;
-    uint256 public a;
+ 
+
+  string[] public candidateList;
+  
+  event VotesReceived(address user , string candidate);
+
+  constructor(string[] memory candidateNames) public {
+    candidateList = candidateNames;
+  }
+
+ 
+  function totalVotesFor(string memory candidate) public view returns (uint256) {
+    return votesReceived[candidate];
+  }
+
+  function voteForCandidate(string memory candidate) public {
+    votesReceived[candidate] += 1;
     
-    constructor(uint256 _a) public { //blue is read only  _a have is u need u and u complete it hide and remove 
-        a = _a;
-    }
-    
-    
-    
-    function plus(uint256 b) //yellow write
-        public 
-        view //not edit in value or this is read only if u not use it , it is yellow
-        returns(uint256){
-        return a+b;
-    }
-    
-    function modify(uint256 newA) public { //function write
-        a = newA;
-    }
+    emit VotesReceived(msg.sender,candidate);
+  }
+  
+  function candidateCount() public view returns (uint256) {
+      return candidateList.length;
+  }
 }
-//0x692a70D2e424a56D2C6C27aA97D1a86395877b3A
-//0xbBF289D846208c16EDc8474705C748aff07732dB
